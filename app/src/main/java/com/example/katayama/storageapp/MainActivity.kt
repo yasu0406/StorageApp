7869c9fce662af4251d67c5adc3ca5ff5c6b298b
katayama <yasucom4646@gmail.com>
2018-09-26 07:35:15 -0700

    add upload method

--- app/src/main/java/com/example/katayama/storageapp/MainActivity.kt
+++ app/src/main/java/com/example/katayama/storageapp/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.katayama.storageapp
 
-import android.app.Activity
 import android.app.ProgressDialog
-import android.content.ContentResolver
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
@@ -19,13 +17,15 @@ import android.webkit.MimeTypeMap
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
-import com.google.android.gms.tasks.OnSuccessListener
+import android.widget.Toast
+import com.example.katayama.storageapp.model.ImageUploadInfo
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageMetadata
 import com.google.firebase.storage.StorageReference
-import com.google.firebase.storage.UploadTask
 import java.io.IOException
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -57,8 +57,11 @@ class MainActivity : AppCompatActivity() {
                     .setAction("Action", null).show()
         }
 
+        // Assign FirebaseStorage instance to storageReference.
         storageReference = FirebaseStorage.getInstance().reference
-        databaseReference = FirebaseDatabase.getInstance().reference
+
+        // Assign FirebaseDatabase instance with root database name.
+        databaseReference = FirebaseDatabase.getInstance().getReference(Database_Path)
 
         //Assign ID'S to button.
         ChooseButton = findViewById(R.id.ButtonChooseImage) as Button
@@ -72,18 +75,20 @@ class MainActivity : AppCompatActivity() {
 //        progressDialog = ProgressDialog(MainActivity.this)
 
 
-        ChooseButton!!.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(view: View) {
-                // Creating intent
-                var intent = Intent()
+        ChooseButton!!.setOnClickListener { view ->
+            // Creating intent
+            var intent = Intent()
 
-                // Setting intent type as image to select image from phone storage.
-                intent.setType("image/*")
-                intent.setAction(Intent.ACTION_GET_CONTENT)
-                startActivityForResult(Intent.createChooser(intent, "Please Select Image"), Image_Request_Code)
-            }
-        })
+            // Setting intent type as image to select image from phone storage.
+            intent.setType("image/*")
+            intent.setAction(Intent.ACTION_GET_CONTENT)
+            startActivityForResult(Intent.createChooser(intent, "Please Select Image"), Image_Request_Code)
+        }
 
+        UploadButton!!.setOnClickListener { view ->
+            // Calling method to upload selected image on Firebase storage.
+            UploadImageFileToFirebaseStorage()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -127,7 +132,7 @@ class MainActivity : AppCompatActivity() {
 
     // Creating Method to get the selected image file Extension from File Path URI.
     fun GetFileExtension(uri: Uri):String {
-        var contentResolver: ContentResolver = getContentResolver()
+        val contentResolver = getContentResolver()
 
         var mimeTypeMap: MimeTypeMap = MimeTypeMap.getSingleton()
 
@@ -145,17 +150,40 @@ class MainActivity : AppCompatActivity() {
             // Showing progressDialog.
 
             // Creating second StorageReference.
-            var storageReference2nd: StorageReference = storageReference!!.child(Storage_Path + System.currentTimeMillis() + "." + GetFileExtension(FilePathUri!!))
+            val storageReference2nd = storageReference!!.child(Storage_Path + UUID.randomUUID().toString())
+            // Create the file metadata
+            var metadata = StorageMetadata.Builder()
+                    .setContentType("image/jpeg")
+                    .build()
             // Adding addOnSuccessListener to second StorageReference.
-            storageReference2nd.putFile(FilePathUri!!).addOnSuccessListener{ object : OnSuccessListener<UploadTask.TaskSnapshot>{
+            storageReference2nd.putFile(FilePathUri!!,metadata).addOnSuccessListener{ taskSnapshot ->
 
-                override fun onSuccess(task: UploadTask.TaskSnapshot) {
-                    // Getting image name from EditText and store into string variable.
-                    var TempImageName = ImageName!!.text.toString().trim()
-                }
-            }
+                // Getting image name from EditText and store into string variable.
+                var TempImageName = ImageName!!.text.toString().trim()
 
+                // Showing toast message after done uploading.
+                Toast.makeText(applicationContext, "Image Uploaded Successfully ", Toast.LENGTH_SHORT).show()
+
+                @SuppressWarnings("VisibleForTests")
+                var imageUploadInfo = ImageUploadInfo(TempImageName, taskSnapshot.storage.downloadUrl.toString())
+
+                // Getting image upload ID.
+                var ImageUploadId:String? = databaseReference!!.push().key
+
+                // Adding image upload id s child element into databaseReference.
+                databaseReference!!.child(ImageUploadId!!).setValue(imageUploadInfo)
+
+            }.addOnFailureListener{ exception ->
+
+                // Showing exception erro message.
+                Toast.makeText(this@MainActivity, exception.message, Toast.LENGTH_SHORT).show()
+
+            }.addOnProgressListener { taskSnapshot ->
+                // taskSnapshot.bytesTransferred
+                // taskSnapshot.totalByteCount
             }
+        } else {
+            Toast.makeText(this@MainActivity, "Please Select Image or Add Image Name", Toast.LENGTH_SHORT).show()
         }
 
     }
