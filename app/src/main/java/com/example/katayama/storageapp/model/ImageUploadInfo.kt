b325a31848e4e1bb82357534381533dfec24fa78
katayama <yasucom4646@gmail.com>
2018-09-29 20:52:14 -0700

    remake field

--- app/src/main/java/com/example/katayama/storageapp/model/ImageUploadInfo.kt
+++ app/src/main/java/com/example/katayama/storageapp/model/ImageUploadInfo.kt
@@ -1,19 +1,3 @@
 package com.example.katayama.storageapp.model
 
-class ImageUploadInfo {
-
-    var imageName: String? = null
-
-    var imageURL: String? = null
-
-    constructor() {
-
-    }
-
-    constructor(name: String, url: String) {
-
-        this.imageName = name
-        this.imageURL = url
-    }
-
-}
+class ImageUploadInfo(var imageName: String?, var imageURL: String?)
