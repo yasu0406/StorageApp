2696302e31eaed88af091e90a18b04ba0857630f
katayama <yasucom4646@gmail.com>
2018-10-18 20:30:27 -0700

    add viewpageradapter

--- app/src/main/java/com/example/katayama/storageapp/ListDetailActivity.kt
+++ app/src/main/java/com/example/katayama/storageapp/ListDetailActivity.kt
@@ -5,34 +5,37 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
+import com.example.katayama.storageapp.adapter.ViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_list_detail.*
 
 
 class ListDetailActivity : AppCompatActivity() {
 
     private var imageDetail: ImageView? = null
     private var title: TextView? = null
-    private var content: TextView? = null
+
+    internal lateinit var viewpageradapter: ViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_detail)
+        viewpageradapter = ViewPagerAdapter(supportFragmentManager)
+
+        this.viewPager.adapter = viewpageradapter
+        this.tab_layout.setupWithViewPager(this.viewPager)
 
         val extras = intent.extras
 
         var imageURL = extras.get("imageURL")
         var imageName = extras.get("imageName")
-        var imageContent = extras.get("imageContent")
-
 
         imageDetail = findViewById(R.id.imageDetail)
         title = findViewById(R.id.title)
-        content = findViewById(R.id.content)
 
         Glide.with(imageDetail)
                 .load(imageURL)
                 .into(imageDetail)
         title!!.setText(imageName as String)
-        content!!.setText(imageContent as String)
 
 
     }
