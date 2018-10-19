1f839f21843ba3c5809e9c75e15d99bd41e600f4
katayama <yasucom4646@gmail.com>
2018-10-18 20:29:41 -0700

    add floatingActionButton

--- app/src/main/java/com/example/katayama/storageapp/ReviewsFragment.kt
+++ app/src/main/java/com/example/katayama/storageapp/ReviewsFragment.kt
@@ -1,7 +1,9 @@
 package com.example.katayama.storageapp
 
+import android.content.Intent
 import android.support.v4.app.Fragment
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,13 +13,21 @@ import android.widget.TextView
 class ReviewsFragment: Fragment() {
     var detailText: TextView? = null
     var relMain: RelativeLayout? = null
+    var floatingActionButton: FloatingActionButton? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view: View? = inflater!!.inflate(R.layout.fragment_detail, container, false)
+        var view: View? = inflater!!.inflate(R.layout.fragment_reviews, container, false)
 
         relMain = view?.findViewById(R.id.rel_main) as RelativeLayout
         detailText = view?.findViewById(R.id.detailTextView) as TextView
+        floatingActionButton = view!!.findViewById(R.id.floatingActionButton) as FloatingActionButton
         detailText!!.text = "test2"
+
+        floatingActionButton!!.setOnClickListener {view ->
+            var intent = Intent(view.context, ReviewWriteActivity::class.java)
+            intent.putExtra("imageContent", "test")
+            startActivity(intent)
+        }
         return view
     }
 }
